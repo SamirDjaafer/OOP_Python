253760a18455638a413bf7147f84e84c8d060d83
Samir <samirateeqdjaafer30@gmail.com>
2020-05-29 15:59:57 +0100

    Cat class updated with meow, eat, sleep, purr methods

--- class_constructor.py
+++ class_constructor.py
@@ -1,9 +1,29 @@
 class Cat:
 
-    def __init__(self, breed = "", colour = ""):
+    def __init__(self, breed="", colour="", hunger_bar="full", tiredness="awake", age=int("")):
         self.breed = breed
         self.colour = colour
+        self.hunger_bar = hunger_bar
+        self.tiredness = tiredness
+        self.age = age
 
     def meow(self):
+        self.tiredness = "tired"
         return "Meow!"
 
+    def eat(self):
+        if self.hunger_bar.lower() == "full":
+            return "I'm already full! MEOW."
+        elif self.hunger_bar.lower() != "full":
+            self.hunger_bar = "full"
+            return "MMM.. Food.. YUM!"
+
+    def sleep(self):
+        if self.tiredness.lower() == "awake":
+            return "Meow, im not tired enough to sleep"
+        elif self.tiredness.lower() == "tired":
+            self.tiredness = "awake"
+            return "ZZzzZZzzZZzz"
+
+    def purr(self):
+        return "Purrrrrrrr"
