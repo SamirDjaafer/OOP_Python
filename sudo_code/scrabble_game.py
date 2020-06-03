a4da6a0dc36f9ca96bc07ddf67e0cb0ed73a1b3d
Samir <samirateeqdjaafer30@gmail.com>
2020-06-03 16:28:56 +0100

    all excercises complete

--- sudo_code/scrabble_game.py
+++ sudo_code/scrabble_game.py
@@ -1,22 +1,21 @@
-print("This program will calculate the scrabble value for any given word\n")
-again = 'Y'
-while again == 'Y':
+replay = 'Y'
+while replay == 'Y':
     word = input("Enter a word:    ").lower()
     score = 0
-    for w in word:
-        if w in 'aeioulnstr':
+    for letter in word:
+        if letter in 'aeioulnstr':
             score += 1
-        elif w in 'dg':
+        elif letter in 'dg':
             score += 2
-        elif w in 'bcmp':
+        elif letter in 'bcmp':
             score += 3
-        elif w in 'fhvwy':
+        elif letter in 'fhvwy':
             score += 4
-        elif w == 'k':
+        elif letter == 'k':
             score += 5
-        elif w in 'jx':
+        elif letter in 'jx':
             score += 8
-        elif w in 'qz':
+        elif letter in 'qz':
             score += 10
     print(f"The score for this word is: {score}")
-    again = input("Go again? (Y/N)     ").strip().upper()
+    replay = input("Play Again? (Y/N)     ").strip().upper()
